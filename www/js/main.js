232b1953b10fb5688ec483f2e95348ec5c8cde76
Annie Hsu <anniesheepy@gmail.com>
2017-11-03 13:18:28 -0700

    add restart button

--- www/js/main.js
+++ www/js/main.js
@@ -10,16 +10,10 @@ $(function(){
 	});		
 });
 
-
-
-$(function(){
-  // Bind the swipeleftHandler callback function to the swipe event on div.box
-  $( "div.box" ).on( "swipeleft", swipeleftHandler );
- 
-  // Callback function references the event target and adds the 'swipeleft' class to it
-  function swipeleftHandler( event ){
-    $( event.target ).addClass( "swipeleft" );
-  }
+$(function(){	
+	$("button").on("tap",function(){
+	  location.reload();
+	});		
 });
 
         var IMG_WIDTH = 500;
